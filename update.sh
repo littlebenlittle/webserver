682f2a081c4d2690b600ff64344ff4eeda206293
ben little <ben.little@benlittle.dev>
2022-05-03 18:10:28 -0600

    use proper subdamin

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sed -e "s|REDIRECT|https://$1.dweb.link/|" ./src/index.html > benlittledev.com/index.html
+sed -e "s|REDIRECT|https://$1.ipfs.dweb.link/|" ./src/index.html > benlittledev.com/index.html
 ./build.sh
 ./run.sh nginx
