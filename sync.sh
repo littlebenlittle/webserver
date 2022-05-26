427ff4ce4dbe5704e49e59c336598d382d1257cb
ben little <ben.little@benlittle.dev>
2022-05-26 13:52:01 -0600

    update which folders sync

--- sync.sh
+++ sync.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 if [ -z "$REMOTE_DIR" ]; then echo "set REMOTE_DIR"; exit 1; fi
-if [ -z "$1" ]; then echo "use $0 <file or dir>"; exit 1; fi
 
-rsync -avz "$1" "$REMOTE_DIR"
+rsync -avz --files-from ./include  . "$REMOTE_DIR"
