147f6b08f398460a12ce8abbf6beac369b193942
ben little <ben.little@benlittle.dev>
2022-08-10 14:19:31 -0600

    redirect to specific pages

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+ctr_runtime=${CTR_RUNTIME:-podman}
+
 case "$1" in
     'nginx')
         if [ -z "$2" ]; then echo "use: $0 nginx <CID>"; exit 1; fi
@@ -7,11 +9,11 @@ case "$1" in
         target="https://gateway.pinata.cloud/ipfs/$cid"
         cd ./nginx || exit 1
         sed -e "s|REDIRECT_TARGET|$target|" nginx.tmpl.conf >nginx.conf
-        podman build -t nginx .
+        $ctr_runtime build -t nginx .
     ;;
     'ipfs')
         cd ./ipfs || exit 1
-        podman build -t ipfs .
+        $ctr_runtime build -t ipfs .
     ;;
     *)
         echo "use:"
@@ -20,4 +22,3 @@ case "$1" in
         echo "$0 ipfs"
     ;;
 esac
-
