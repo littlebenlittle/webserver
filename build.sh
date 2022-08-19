4892b13f8e8b116d63de84a043ca006a56aa8d27
ben little <ben.little@benlittle.dev>
2022-08-19 13:34:06 -0600

    stop using pinata

--- build.sh
+++ build.sh
@@ -6,9 +6,8 @@ case "$1" in
     'nginx')
         if [ -z "$2" ]; then echo "use: $0 nginx <CID>"; exit 1; fi
         cid="$2"
-        target="https://gateway.pinata.cloud/ipfs/$cid"
         cd ./nginx || exit 1
-        sed -e "s|REDIRECT_TARGET|$target|" nginx.tmpl.conf >nginx.conf
+        sed -e "s|CID|$cid|" nginx.tmpl.conf >nginx.conf
         $ctr_runtime build -t nginx .
     ;;
     'ipfs')
--- run.sh
+++ run.sh
@@ -1,24 +1,29 @@
 #!/bin/sh
 
+podman pod ls | grep web >/dev/null || podman pod create \
+    --name web \
+    -p 80:80 \
+    -p 4001:4001 \
+    -p 127.0.0.1:5001:5001 \
+    -p 127.0.0.1:8080:8080
+
 case "$1" in
     'nginx')
         podman stop nginx && podman rm nginx
         podman run -d \
+            --pod web \
             --restart=always \
             --name nginx \
-            -p 80:80 \
             nginx
     ;;
     'ipfs')
         podman stop ipfs && podman rm ipfs
         podman run -d \
+            --pod web \
             --restart always \
             --name ipfs \
             -v ipfs-data:/data/ipfs \
             -v /staging:/staging:ro \
-            -p 4001:4001 \
-            -p 127.0.0.1:5001:5001 \
-            -p 127.0.0.1:8080:8080 \
             ipfs
     ;;
 esac
